8a08c16ca680ce4b6d9f30bc199bd60e79bd48a4
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2019-08-17 23:32:49 +0000

    Added the user model

--- SourceFiles/profiles_project/profiles_api/models.py
+++ SourceFiles/profiles_project/profiles_api/models.py
@@ -2,5 +2,36 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUSer
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+
+class UserProfile(AbstractBaseUSer, PermissionsMixin):
+    """Represents a user profile inside this system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    isActive = models.BooleanField(default=True)
+    isStaff = models.BooleanField(default=False)
+
+    """The below is an object manager"""
+    object = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def getFullName(self):
+        """This function returns a user's full name"""
+        return self.name
+
+    def getShortName(self):
+        """This function returns a short name for the user"""
+        shortName = f"{self.firstName} {self.lastName}"
+        return shortName
+
+    def __str__(self):
+        """Django uses this to convert the object into a nice readable form"""
+        return self.email
