8c261f3e10e4e923b5f19a77aa46de2e328bc11d
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2019-08-19 09:55:17 +0200

    Commented degug mode in sjango settings

--- SourceFiles/profiles_project/profiles_project/settings.py
+++ SourceFiles/profiles_project/profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b7w#^++bb2olplg3vrsscg4c3snn0_%o3e0ub@u_1frprzjg99'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
